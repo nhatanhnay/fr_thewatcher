4fe6916f713de37576820bf3bd85074fb4ce7c86
Na <lecongnhatanh@gmail.com>
2024-10-14 18:59:19 +0700

    update 7

--- src/app/functions/data.ts
+++ src/app/functions/data.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import { catchError } from 'rxjs/operators';
 
 interface WatchDetails {
-    id: number,
-    linkHref: string,      // Chú ý thay đổi từ LinkHref sang linkHref
-    name: string,          // Chú ý thay đổi từ Name sang name
-    price: number,
-    rating: number,
-    urlImage: string,      // Chú ý thay đổi từ UrlImage sang urlImage
-    numberRating: number    // Chú ý thay đổi từ NumberRating sang numberRating
+    id: number;
+    linkHref: string;      // Chú ý thay đổi từ LinkHref sang linkHref
+    name: string;          // Chú ý thay đổi từ Name sang name
+    price: number;
+    rating: number;
+    urlImage: string;      // Chú ý thay đổi từ UrlImage sang urlImage
+    numberRating: number;  // Chú ý thay đổi từ NumberRating sang numberRating
 }
 
 interface CartItem {
@@ -36,13 +36,24 @@ interface OrderDetails {
 @Injectable({
     providedIn: 'root',
 })
-
 export class DataService {
     private apiUrl = 'http://103.81.87.196:4699/watch-details';
 
     constructor(private http: HttpClient) { }
 
+    // Phương thức lấy chi tiết đồng hồ
     getWatchDetails(): Observable<WatchDetails[]> {
-        return this.http.post<WatchDetails[]>(this.apiUrl, {});
+        // Định nghĩa headers với Content-Type
+        const headers = new HttpHeaders({
+            'Content-Type': 'application/json', // Đặt Content-Type là application/json
+        });
+
+        return this.http.post<WatchDetails[]>(this.apiUrl, {}, { headers }).pipe(
+            catchError((error) => {
+                console.error('Error fetching watch details', error);
+                return throwError(error);
+            })
+        );
     }
 }
+
