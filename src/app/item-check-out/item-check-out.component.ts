10919e8da95cf0658b89e957d25d9e6fcf108db8
Na <lecongnhatanh@gmail.com>
2024-10-21 11:13:43 +0700

    hehe

--- src/app/item-check-out/item-check-out.component.ts
+++ src/app/item-check-out/item-check-out.component.ts
@@ -12,7 +12,7 @@ export class ItemCheckOutComponent {
   @Input() name: string = '';
   @Input() price: number = 0;
   @Input() quantity: number = 0;
-
+  totalPrice = this.price * this.quantity;
   formatPrice(value: number): string {
     return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "."); // Định dạng với dấu phân cách
   }
